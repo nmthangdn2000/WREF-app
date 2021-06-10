9598f8c8efd5049018c0df3c79107f4d4ce2b573
Uptimum <uptimumdn2000@gmail.com>
2021-06-10 21:56:04 +0700

    socket.io commit

--- app/src/main/java/thang/com/wref/util/SocketIO.java
+++ app/src/main/java/thang/com/wref/util/SocketIO.java
@@ -8,7 +8,9 @@ import io.socket.client.Socket;
 import static thang.com.wref.util.Constants.BASE_URL;
 
 public class SocketIO {
+
     public static Socket socket;
+
     public void ConnectSocket(){
         try {
             socket = IO.socket(BASE_URL);
